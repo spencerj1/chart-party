e15e3678278a00f2ddd2a85687921352c5e24637
James Spencer <jamesspencer348@gmail.com>
2020-04-04 14:43:20 -0400

    getting started

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const url = require('url');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) { 
+  res.status(200).sendFile('./html/index.html', { root: __dirname })
+});
+
+app.get('/js/us-map', function (req, res) { 
+  res.status(200).sendFile('./js/maps/us.map.js', { root: __dirname })
+});
+
+app.get('/js/highcharts-theme', function (req, res) { 
+  res.status(200).sendFile('./js/themes/highcharts-theme.js', { root: __dirname })
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+ 
+app.listen(port, function () {
+  console.log('App started at http://localhost:' + port);
+});
+
---
+++ js/maps/us.map.js
@@ -0,0 +1,67 @@
+Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/us-population-density.json', function (data) {
+
+    // Make codes uppercase to match the map data
+    data.forEach(function (p) {
+        p.code = p.code.toUpperCase();
+    });
+
+    // Instantiate the map
+    Highcharts.mapChart('container', {
+
+        chart: {
+            map: 'countries/us/us-all'
+        },
+
+        title: {
+            text: 'United States COVID-19 Cases'
+        },
+
+        exporting: {
+            sourceWidth: 600,
+            sourceHeight: 600
+        },
+
+        legend: {
+            layout: 'vertical',
+            borderWidth: 0,
+            backgroundColor: '#2a2a2b',
+            floating: true,
+            horizontalAlign: 'left',
+            y: -30,
+            x: 290
+        },
+
+        mapNavigation: {
+            enabled: false
+        },
+
+        colorAxis: {
+            min: 1,
+            type: 'logarithmic',
+            minColor: '#ffffff',
+            maxColor: '#ff0000',
+            stops: [
+                [0, '#ffffff'],
+                [0.67, '#ff8a8a'],
+                [1, '#ff0000']
+            ]
+        },
+
+        series: [{
+            animation: {
+                duration: 1000
+            },
+            data: data,
+            joinBy: ['postal-code', 'code'],
+            dataLabels: {
+                enabled: true,
+                color: '#FFFFFF',
+                format: '{point.code}'
+            },
+            name: 'Population density',
+            tooltip: {
+                pointFormat: '{point.code}: {point.value} Cases'
+            }
+        }]
+    });
+});
